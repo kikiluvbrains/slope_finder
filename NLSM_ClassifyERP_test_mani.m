7b9a19dc743e37e2f2d556fceb46005461c839a5
Kinkini <121206270+kikiluvbrains@users.noreply.github.com>
2025-01-29 14:36:11 -0500

    Add files via upload
    with orientation

--- NLSM_ClassifyERP_test_mani.m
+++ NLSM_ClassifyERP_test_mani.m
@@ -3,6 +3,7 @@ function NLSM_ClassifyERP_test_mani
 % Decode shape and "colour"(material) of objects across time
 
 %% Define analysis
+noerps = 6
 subNum = 1:14; %should be 16 but you forgot to transfer some data for 15 and 16
 events = 1;%1=align to Preview Onset; 3=align to Movement Onset
 eeglab;
@@ -10,13 +11,11 @@ eeglab;
 % Get participants
 nSubs = length(subNum);
 for i = 1:nSubs
-    if i ~= 0
-        subjects{i} = ['p', num2str(subNum(i))];
-    end
+    subjects{i} = ['p', num2str(subNum(i))];
 end
-noerps = 3
+
 % Set temporal window
-nCat = 5; windsorize = 1; normalize = 1; sloper = 0;
+nCat = 5; windsorize = 1; normalize = 1;
 %% Analysis Loop
 % select event alignment
 for ievent = 1:length(events)
@@ -24,15 +23,14 @@ for ievent = 1:length(events)
     accurate = [];
     % select subjects
 for iSub = 1:length(subjects)
-    if iSub ~= 0       
         disp(['Subject Number: ',num2str(iSub)]);
         disp('Getting ERPs...');
         nBlocks = 24;
         
         %% Get ERPs
-        if eventalign == 1
-            load(['/Users/Owner/Downloads/Niemier - SVM analysis/Matlab_Nina/data/premade_erps/',subjects{iSub},'/ERPs_mnn_Aug27.mat']);
-        end
+            if eventalign == 1
+                load(['/home/kinkini/Downloads/Neuroscience/Matthias/Leenina/Data/',subjects{iSub},'/ERPs_mnn_Aug27.mat']);
+            end
         %% Concatenate data
         disp('concatenating data...');
         tic
@@ -46,17 +44,17 @@ for iSub = 1:length(subjects)
             elseif iVar == 4
                 ERPdata = knb;
             end
-           fields=fieldnames(ERPdata);
-           ERPdata.ob1 = ERPdata.ob1(1:noerps,:,:)
-           ERPdata.ob2 = ERPdata.ob2(1:noerps,:,:)
-           ERPdata.ob3 = ERPdata.ob3(1:noerps,:,:)
-           ERPdata.ob4 = ERPdata.ob4(1:noerps,:,:)
 
-           for categoryidx = 1:length(fields)
-                categoryname = fields{categoryidx};
-                data = ERPdata.(categoryname); % Original data: (trials x channels x timepoints)
-                tempdata = []; newdata = [];
-                if sloper ~= 1
+           ERPdata.ob1 = ERPdata.ob1(noerps,:,:)
+           ERPdata.ob2 = ERPdata.ob2(noerps,:,:)
+           ERPdata.ob3 = ERPdata.ob3(noerps,:,:)
+           ERPdata.ob4 = ERPdata.ob4(noerps,:,:)
+           fields=fieldnames(ERPdata);
+           for categoryidx=1:length(fields)
+               categoryname=fields{categoryidx};
+                   data = ERPdata.(categoryname);
+                    % concatenate according to nCat
+                    tempdata = []; newdata = [];
                     for i = 1:size(data,3)
                         tempdata = cat(2, tempdata, data(:,:,i));
                         if mod(i,nCat) == 0
@@ -64,52 +62,9 @@ for iSub = 1:length(subjects)
                             tempdata = [];
                         end
                     end
-                else
-                    % Loop over each trial
-                    for iTrial = 1:size(data, 1)
-                        trialData = squeeze(data(iTrial, :, :)); % Extract data: (channels x timepoints)
-                        
-                        % Calculate the number of segments based on the timepoints
-                        numSegments = floor(size(trialData, 2) / nCat); % Number of non-overlapping segments
-                        
-                        % Initialize transformed data (features x segments)
-                        numChannels = size(trialData, 1);
-                        numFeaturesPerChannel = 3; % 3 coefficients for a quadratic polynomial
-                        transformedData = zeros(numChannels * numFeaturesPerChannel, numSegments);
-                        
-                        % Loop over each channel
-                        for iChan = 1:numChannels
-                            channelData = trialData(iChan, :); % Extract 1D array: (1 x timepoints)
-                            featureMatrix = []; % Store polynomial coefficients for this channel
-                    
-                            % Loop over non-overlapping groups of 5 timepoints
-                            for iTime = 1:nCat:length(channelData) - nCat + 1
-                                % Get the current window of timepoints (5 points)
-                                timeWindow = channelData(iTime:iTime + nCat - 1); % Window of 5 points
-                    
-                                % Fit a quadratic polynomial (degree = 2)
-                                x = 1:nCat; % Time indices for the window
-                                p = polyfit(x, timeWindow, 2); % Polynomial coefficients (3 values)
-                    
-                                % Store the polynomial coefficients (3 values for quadratic)
-                                featureMatrix = [featureMatrix, p]; % Append coefficients (3 values)
-                            end
-                            
-                            % Reshape the feature matrix to 3 rows (coefficients) and numSegments columns (windows)
-                            featureMatrix = reshape(featureMatrix, numFeaturesPerChannel, numSegments);
-                    
-                            % Store coefficients for this channel: 3 coefficients per window, numSegments windows
-                            transformedData((iChan - 1) * numFeaturesPerChannel + 1:iChan * numFeaturesPerChannel, :) = featureMatrix;
-                        end
-                    
-                        % Store the transformed trial data
-                        newdata(iTrial, :, :) = transformedData; % Shape: (1 trial x (numChannels * numFeaturesPerChannel) features x numSegments)
-                    end
-                end
-                % Update ERPdata structure with the transformed data
-                ERPdata.(categoryname) = newdata; % Final shape: (6 trials x 192 features x 133 segments)
-            end
-        
+                   ERPdata.(categoryname)=newdata;
+           end
+ 
             if iVar == 1
                 cw = ERPdata;
             elseif iVar == 2
@@ -138,22 +93,19 @@ for iSub = 1:length(subjects)
         accurate.Grasp.Colour(iSub,:) = ClassifyERP(grasp_colour_data, grasp_colour_data);
         accurate.Knuckle.Colour(iSub,:) = ClassifyERP(knuckle_colour_data, knuckle_colour_data);
 
+        accurate.Grasp.Orientation(iSub,:) = ClassifyERP(grasp_orientation_data, grasp_orientation_data);
+        accurate.Knuckle.Orientation(iSub,:) = ClassifyERP(kuckle_orientation_data, kuckle_orientation_data);
+
         toc        
         %% save results
         if eventalign == 1
-            savepath = ['/Users/Owner/Downloads/Niemier - SVM analysis/Matlab_Nina/results/time', num2str(nCat),'/'];
+            savepath = '/home/kinkini/Downloads/Neuroscience/Matthias/Leenina/Analyzed';
         end
         
         cd(savepath);
-        if sloper ~= 1
-            save NLSM_ClassifyFeaturesAction_ERP_3.mat -mat accurate MyInfo
-        else
-            save NLSM_ClassifyFeaturesAction_ERP_3_slope.mat -mat accurate MyInfo
-
-        end
-    end % subs
+        save NLSM_ClassifyFeaturesAction_ERP_manis_5.mat -mat accurate MyInfo
+end % subs
 end % event
-end
 end
 
 function [acc] = ClassifyERP(ERP1, ERP2)
@@ -168,6 +120,8 @@ function [acc] = ClassifyERP(ERP1, ERP2)
 % same data.
 % 
 % Apply slope finder to ERP1 and ERP2
+ERP1 = applySlope(ERP1, 1);
+ERP2 = applySlope(ERP2, 1);
 
 nFold = size(ERP1,1)/2;
 grouplabels = [ones(nFold,1); ones(nFold,1).*(-1)]; % changed from 1 & 2 but makes no difference
